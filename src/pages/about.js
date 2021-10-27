4f0acabea4e119b2cf7eea97151c4b871d14e6b6
Ridvan Tahir <s114074ap.be>
2021-10-27 14:44:15 +0200

    Finished React in Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Link } from "gatsby"
+
+const aboutPage = () => {
+    return (
+        <main>
+            <title>About Us</title>
+            <h1>About Us</h1>
+            <Link to="/">Back to Home</Link>
+            <p>Zululu Agency was found in 2021</p>
+        </main>
+    )
+}
+
+export default aboutPage;
---
+++ src/pages/gallery.js
@@ -0,0 +1,11 @@
+import Frame from '../components/frame'
+import React from 'react'
+
+const GalleryPage = () => {
+    return (
+        <Frame>
+            <p> Dir wordt doorgegeven als childeren</p>
+        </Frame>
+        )
+}
+export default GalleryPage
